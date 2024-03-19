3614172bcc93d19a03fa0e3a8d1da6c9ff3ba319
SeniorIntern <tnikhil.official@gmail.com>
2024-03-19 10:18:31 +0545

    Add jwt on POST signin

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,9 @@
 const { StatusCodes } = require('http-status-codes');
+
+const { Auth } = require('../utils/common');
 const { UserRepository } = require('../repositories');
 const AppError = require('../utils/errors/app-error');
+
 const userRepo = new UserRepository();
 
 async function create(data) {
@@ -26,6 +29,32 @@ async function create(data) {
   }
 }
 
+async function signin(data) {
+  try {
+    const user = await userRepo.getUserByEmail(data.email);
+    if (!user) {
+      throw new AppError(
+        'No user found for the given email',
+        StatusCodes.NOT_FOUND
+      );
+    }
+    console.log(data.password, user.password);
+    const passwordMatch = Auth.checkPassword(data.password, user.password);
+    if (!passwordMatch) {
+      throw new AppError('Invalid password', StatusCodes.BAD_REQUEST);
+    }
+    const jwt = Auth.createToken({ id: user.id, email: user.email });
+    return jwt;
+  } catch (error) {
+    console.log(error);
+    throw new AppError(
+      'Something went wrong',
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
 module.exports = {
-  create
+  create,
+  signin
 };
