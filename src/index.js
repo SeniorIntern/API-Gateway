1f1daea4d944dce6d7f5a61dac010ec0c1d42128
SeniorIntern <tnikhil.official@gmail.com>
2024-03-26 13:18:53 +0545

    Add rate limiter and proxy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { rateLimit } = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
@@ -7,6 +9,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  limit: 3 // Limit each IP to 3 requests per `window`
+});
+
+app.use(limiter); // Apply the rate limiting middleware to all requests.
+
+app.use(
+  '/flightsService',
+  createProxyMiddleware({ target: ServerConfig.FLIGHT_SERVICE, changeOrigin: true })
+);
+app.use(
+  '/bookingService',
+  createProxyMiddleware({ target: ServerConfig.BOOKING_SERVICE, changeOrigin: true })
+);
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 
 const { InfoController } = require('../../controllers');
-const { AuthRequestMiddlewares } = require('../../middlewares');
 const userRoutes = require('./user-routes');
 const router = express.Router();
 
-router.get('/info', AuthRequestMiddlewares.checkAuth, InfoController.info);
+router.get('/info', InfoController.info);
 router.use('/user', userRoutes);
 
 module.exports = router;
